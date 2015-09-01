e0eb1a9bdb79ad0b3054e3d8f999e998438e1dd9
Victor <monstervictor@gmail.com>
2015-09-01 19:45:10 -0400

    Merging

---
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Controllers/MeleeAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class MeleeAttack : ExtendedMonobehaviour
+{
+    [SerializeField] 
+    private Platformer2DUserControl _inputManager;
+    [SerializeField] 
+    private GameObject _swingerCollider;
+    [SerializeField] 
+    private Transform _swingerColliderInitialRotation;
+    [SerializeField]
+    private Transform _swingerColliderFinalRotation;
+
+    public float rotationSpeed = 100f;
+    public float startingRotation = -45;
+    public float endingRotation = 45;
+
+    // Use this for initialization
+    private void Start()
+    {
+        if (_inputManager == null) _inputManager = GetComponent<Platformer2DUserControl>();
+        SetRotationEulerZ(startingRotation);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_swingerCollider.transform.rotation.eulerAngles.z <= endingRotation)
+        {
+            SetRotationEulerZ(startingRotation);
+        }
+        else
+        {
+            _swingerCollider.transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+        }
+        
+    }
+
+
+
+}
---
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/GarbageScripts/ExtendedMonobehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ExtendedMonobehaviour : MonoBehaviour, IGameObject {
+
+    protected void SetRotationEulerX(float xValue)
+    {
+        transform.rotation = Quaternion.Euler(xValue, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+
+    protected void SetRotationEulerY(float yValue)
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, yValue, transform.rotation.eulerAngles.z);
+    }
+
+    protected void SetRotationEulerXY(float xValue, float yValue)
+    {
+        transform.rotation = Quaternion.Euler(xValue, yValue, transform.rotation.eulerAngles.z);
+    }
+
+    protected void SetRotationEulerZ(float zValue)
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, zValue);
+    }
+}
