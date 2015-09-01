340e8b216441b33cb97e9d4adf20cd73c06fec0b
symbol24 <phillip.conte@gmail.com>
2015-09-01 19:47:51 -0400

    lets conflict yaaayy \o/

--- UnityFolder/ProjectJuice/Assets/Prototype/Standard Assets/2D/Scripts/Guns/Gun.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Standard Assets/2D/Scripts/Guns/Gun.cs
@@ -2,23 +2,23 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 using GamepadInput;
+using UnityStandardAssets._2D;
 
-public class Gun : MonoBehaviour {
+public class Gun : ExtendedMonobehaviour {
+    public Platformer2DUserControl m_Controller { get; private set; }
 
-    public GameObject m_GunReference;
-    public Bullet m_BulletPrefab;
-    private bool m_Fire1;
-    public float m_ShotDelayFast = 0.1f;
-    public float m_ShotDelayMedium = 0.3f;
+    [SerializeField] private GameObject m_GunReference; //used for flipping maybe
+    [SerializeField] private GameObject m_Gun;
+    [SerializeField] private Bullet m_BulletPrefab;
+    [SerializeField] private float m_ShotDelay = 0.1f;
+    [SerializeField] private float m_BurstDelay = 0.1f;
+    
     private float m_ShotTimer;
-    protected float m_CurrentDelay;
 
-    protected GamePad.Index controller = GamePad.Index.Any;
-    protected GamepadState state;
 
     void Awake()
     {
-        state = GamePad.GetState(controller);
+        m_Controller = GetComponent<Platformer2DUserControl>();
     }
 
     // Use this for initialization
@@ -29,22 +29,15 @@ public class Gun : MonoBehaviour {
 	// Update is called once per frame
 	protected void Update ()
     {
-        m_Fire1 = GamePad.GetButton(GamePad.Button.X, controller);
-    }
-
-    protected void FixedUpdate()
-    {
-        if (m_Fire1 && Time.time > m_ShotTimer)
+        if (m_Controller.m_Shoot && Time.time > m_ShotTimer)
         {
             Fire();
-            m_ShotTimer = Time.time + m_CurrentDelay;
+            m_ShotTimer = Time.time + m_ShotDelay;
         }
     }
 
-
-
     public virtual void Fire()
     {
-        //print("fire");
+        Bullet newBullet = Instantiate(m_BulletPrefab, m_GunReference.transform.position, m_GunReference.transform.rotation) as Bullet;
     }
 }
