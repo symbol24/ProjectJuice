95ab030d9161e7dec80fc7103f276903a8f7aa4e
symbol24 <phillip.conte@gmail.com>
2015-09-01 19:12:10 -0400

    updated dashing

--- UnityFolder/ProjectJuice/Assets/Prototype/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -18,17 +18,12 @@ namespace UnityStandardAssets._2D
         [SerializeField] private float m_DashDelay = 0.5f;                  // Delay before dashing again
         private float m_DashTimer = 0.0f;
 
-
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-
-
-       
-
 
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
@@ -67,6 +62,12 @@ namespace UnityStandardAssets._2D
                 }
             }
 
+            if (m_DashTimer < Time.time && !m_CanDash)
+            {
+                m_CanDash = true;
+                m_AirControl = true;
+            }
+
             // Set whether or not the character is crouching in the animator
             //m_Anim.SetBool("Crouch", dash);
 
@@ -83,21 +84,14 @@ namespace UnityStandardAssets._2D
                 m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !m_Controller.m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
-                    // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && m_Controller.m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
+                if ((move > 0 && !m_Controller.m_FacingRight) || (move < 0 && m_Controller.m_FacingRight)) Flip(); // ... flip the player.
+
             }
+
             // If the player should jump...
             if (m_Grounded && jump && m_Anim.GetBool("Ground")) Jump();
 
+            // If the player should dash
             if (m_CanDash && dash) Dash();
         }
 
@@ -120,29 +114,30 @@ namespace UnityStandardAssets._2D
                 // Add a directional force to the player as per input.
                 m_CanDash = false;
 
-                Vector2 angle = new Vector2(m_Controller.m_XAxis, m_Controller.m_YAxis);
+                Vector2 angle = new Vector2(m_Controller.m_XAxis, m_Controller.m_YAxis); //get dash angle from x axis
 
+                //if no x input set direction to horizontal
                 if (m_Controller.m_XAxis <= 0.1f && m_Controller.m_XAxis >= -0.1f && m_Controller.m_YAxis <= 0.1f && m_Controller.m_YAxis >= -0.1f)
                 {
                     angle.x = 1f;
                     angle.y = 0f;
                 }
 
-                if (m_Grounded) angle.x = 1f;
+                // if grounded force y to positive
+                if (m_Grounded && angle.y < 0) angle.y = -angle.y;
 
+                // if not facing right force x negative
                 if (!m_Controller.m_FacingRight && angle.x > 0) angle.x = -angle.x;
 
+                // normalize and add impulse value
                 angle = angle.normalized * m_DashImpulse;
 
+                //add the force as force
                 m_Rigidbody2D.AddForce(angle, ForceMode2D.Force);
 
+                //set values for cooldown
                 m_AirControl = false;
                 m_DashTimer = Time.time + m_DashDelay;
-            }
-            else if (m_DashTimer < Time.time)
-            {
-                m_CanDash = true;
-                m_AirControl = true;
             }
         }
 
@@ -173,7 +168,7 @@ namespace UnityStandardAssets._2D
 
             if (isGrounded)
             {
-                m_CanDash = true;
+                //m_CanDash = true;
                 m_AirControl = true;
             }
 
