637877c24391ee86a4968d08bbdf324c92666efb
unknown <1411733@5B1-07.ad.dawsoncollege.qc.ca>
2015-09-11 21:12:36 -0400

    BuildingErrorsFixed

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Guns/ArcShooting.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Guns/ArcShooting.cs
@@ -15,23 +15,7 @@ public class ArcShooting : Gun
     protected override void Update() {
         base.Update();
 
-<<<<<<< HEAD
-        m_GunProperties.transform.eulerAngles = GetRotation(m_Controller);
-=======
-        RotateGun(m_Controller.m_XAxis, m_Controller.m_YAxis);
-    }
-
-    private void RotateGun(float xAxis, float yAxis)
-    {
-        if (!m_Controller.m_FacingRight && xAxis > 0) xAxis = -xAxis;
-        if (!m_Controller.m_FacingRight && xAxis == 0 && yAxis == 0) xAxis = -1f;
-
-        float zAngle = Mathf.Atan2(yAxis, xAxis) * Mathf.Rad2Deg;
-        float xAngle = 0f;
-        float yAngle = 0f;
-        
-        m_Gun.transform.eulerAngles = new Vector3(xAngle, yAngle, zAngle);
->>>>>>> 0cd709af66132d1b0da1da45d276d965c3029652
+        m_Gun.transform.eulerAngles = GetRotation(m_Controller);
     }
 
     public override void Fire()
