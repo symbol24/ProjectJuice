155b406d40c75d2fa0d1bcd0852ae2123c0777d4
symbol24 <phillip.conte@gmail.com>
2015-09-27 13:29:52 -0400

    fixed particles to not float yay

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Splasher/SingleParticleGrounder.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Splasher/SingleParticleGrounder.cs
@@ -3,38 +3,19 @@ using System.Collections;
 
 public class SingleParticleGrounder : MonoBehaviour {
 
-    [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
-    private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-    const float k_GroundedRadius = 0.01f; // Radius of the overlap circle to determine if grounded
     private bool m_Grounded = false;            // Whether or not the player is grounded.
     private bool m_Immobile = false;
     private Rigidbody2D m_RigidBody;
 
     // Use this for initialization
     void Start () {
-        m_GroundCheck = transform.FindChild("ParticleGroundCheck");
         m_RigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!m_Grounded) m_Grounded = CheckGrounded();
         if (m_Grounded && !m_Immobile) m_Immobile = SetImmobile();
 	}
-
-    private bool CheckGrounded()
-    {
-        bool isGrounded = false;
-
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject != gameObject)
-                isGrounded = true;
-        }
-
-        return isGrounded;
-    }
 
     private bool SetImmobile()
     {
@@ -45,5 +26,14 @@ public class SingleParticleGrounder : MonoBehaviour {
         isImmobile = true;
 
         return isImmobile;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Ground ground = collider.GetComponent<Ground>();
+        if(ground != null)
+        {
+            m_Grounded = true;
+        }
     }
 }
