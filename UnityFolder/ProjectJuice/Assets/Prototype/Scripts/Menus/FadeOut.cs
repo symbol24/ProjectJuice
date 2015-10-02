e3018408c4711a1f42e6e0e11a27f05eb36d9974
symbol24 <phillip.conte@gmail.com>
2015-10-01 22:01:19 -0400

    round start timer

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/FadeOut.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/FadeOut.cs
@@ -10,10 +10,15 @@ public class FadeOut : MonoBehaviour {
     private bool m_IsDone = false;
     public bool IsDone { get { return m_IsDone; } }
     [SerializeField] int m_LevelToLoad;
+    [SerializeField] bool TestLoading = false;
+    private PlayerSpawner m_spawner;
     // Use this for initialization
     void Start () {
         m_Sprite = GetComponent<SpriteRenderer>();
         m_SpriteColor = m_Sprite.color;
+        if (TestLoading) OnLevelWasLoaded(Application.loadedLevel);
+        m_spawner = FindObjectOfType<PlayerSpawner>();
+        if(m_spawner != null) m_spawner.SpawnDone += M_spawner_SpawnDone;
 	}
 
     void OnLevelWasLoaded(int level)
@@ -21,7 +26,8 @@ public class FadeOut : MonoBehaviour {
         if (level == m_LevelToLoad)
         {
             OnLoadDone();
-            StartCoroutine(FadeObject(false));
+            if(m_spawner == null) //this means theres no spawner object so we can bypass the wait for spawning.
+                StartCoroutine(FadeObject(false));
         }
 
     }
@@ -32,6 +38,7 @@ public class FadeOut : MonoBehaviour {
     {
         EventHandler handler = LoadDone;
         if (handler != null) handler(this, EventArgs.Empty);
+        print("OnLoadDone");
     }
 
     public event EventHandler FadeDone;
@@ -40,6 +47,12 @@ public class FadeOut : MonoBehaviour {
     {
         EventHandler handler = FadeDone;
         if (handler != null) handler(this, EventArgs.Empty);
+        print("OnFadeDone");
+    }
+
+    private void M_spawner_SpawnDone(object sender, EventArgs e)
+    {
+        StartCoroutine(FadeObject(false));
     }
 
     private IEnumerator FadeObject(bool isFadeIn)
--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/RoundStartTimer.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/RoundStartTimer.cs
@@ -4,14 +4,49 @@ using UnityEngine.UI;
 
 public class RoundStartTimer : MonoBehaviour {
     [SerializeField] Text m_TimerText;
+    [SerializeField] PlayerSpawner m_spawner;
+    [Range(0, 3)][SerializeField] float m_delayBetweenNumbers = 1f;
+    [Range(0, 3)][SerializeField] float m_delayAfterGo = 0.5f;
+    [Range(0, 10)][SerializeField] int m_amountOfSeconds = 5;
+    private FadeOut m_fader;
 
 	// Use this for initialization
 	void Start () {
+        if (m_fader == null) m_fader = FindObjectOfType<FadeOut>();
+        if(m_fader != null) m_fader.FadeDone += StartCountdown;
+        m_TimerText.text = Database.instance.GameTexts[16];
+        GameManager.instance.InjectRoundStartTimer(this);
+	}
+
+    private void StartCountdown(object sender, System.EventArgs e)
+    {
+        StartCoroutine(DisplayTime());
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void Reset()
+    {
+        m_TimerText.text = Database.instance.GameTexts[16];
+    }
+
+    IEnumerator DisplayTime()
+    {
+        bool displayed = false;
+        int time = m_amountOfSeconds;
+        while (!displayed)
+        {
+            m_TimerText.text = time.ToString();
+            time--;
+            if (time == 0) displayed = true;
+            yield return new WaitForSeconds(m_delayBetweenNumbers);
+        }
+        m_TimerText.text = Database.instance.GameTexts[17];
+        yield return new WaitForSeconds(m_delayAfterGo);
+        GameManager.instance.SetGameState(GameState.Playing);
+        gameObject.SetActive(false);
+    }
 }
