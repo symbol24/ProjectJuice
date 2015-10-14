954a2a86596f8d8a85ffe6918ead6ab13e8b2472
Victor <monstervictor@gmail.com>
2015-10-13 21:40:12 -0400

    SparklesForCrates

---
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Environment/MovementSparking.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GroundChecker), typeof(MovementChecker))]
+public class MovementSparking : ExtendedMonobehaviour
+{
+    public GameObject _effectsToSpawn;
+    private GroundChecker _groundChecker;
+    private MovementChecker _movementChecker;
+    [SerializeField] private float _sparklesTimeout = 1f;
+    public List<Transform> _pointsOfSparks;
+    private float _groundTolerance = 0.01f;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    _groundChecker = GetComponent<GroundChecker>();
+	    _movementChecker = GetComponent<MovementChecker>();
+        _movementChecker.X_PositionChanged += XPositionChanged;
+	}
+
+    private void XPositionChanged(object sender, PositionChangedEventArgs positionChangedEventArgs)
+    {
+        base.GetPointOfImpact(_groundChecker.PointOfCollision.transform, transform);
+
+        if (_groundChecker.IsGrounded)
+        {
+            var lowestPoint = _pointsOfSparks.Min(c => c.transform.position.y);
+            var pointsCloseToGround =
+                _pointsOfSparks.Where(c => c.transform.position.y < (lowestPoint + _groundTolerance));
+
+
+            foreach (var pointsOfSpark in pointsCloseToGround)
+            {
+                var positionToSpawn = pointsOfSpark.position;
+                var particles =
+                    (GameObject)
+                        Instantiate(_effectsToSpawn,
+                            positionToSpawn,
+                            _effectsToSpawn.transform.rotation);
+                particles.transform.parent = transform;
+                particles.AddComponent<DestroyOnTimer>().Timeout = _sparklesTimeout;
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Utility/GroundChecker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class GroundChecker : ExtendedMonobehaviour {
+
+    public GameObject PointOfCollision { get; private set; }
+    [SerializeField] private float _radius = 1.1f;
+    
+    [SerializeField]
+    private bool _enableDebug = false;
+
+    // Use this for initialization
+    private void Start()
+    {
+        PointOfCollision = new GameObject(Guid.NewGuid().ToString());
+        PointOfCollision.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+        DetectIfGrounded();
+	}
+
+
+    private void DetectIfGrounded()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, _radius);
+        IsGrounded = colliders.Any(c => c.GetComponent<Ground>() != null);
+        if(_enableDebug) print("CheckingIsGrounded " + IsGrounded);
+        
+    }
+
+    private bool _isGrounded;
+    public bool IsGrounded
+    {
+        get { return _isGrounded; }
+        set
+        {
+            if (_isGrounded != value)
+            {
+                _isGrounded = value;
+                OnGroundChanged(new GroundedChangedEventArgs {Grounded = value});
+            }
+        }
+    }
+
+    public EventHandler<GroundedChangedEventArgs> GroundChanged;
+    public void OnGroundChanged(GroundedChangedEventArgs e)
+    {
+        if (GroundChanged != null) GroundChanged(this, e);
+    }
+}
+
+public class GroundedChangedEventArgs : EventArgs
+{
+    public bool Grounded { get; set; }
+}
