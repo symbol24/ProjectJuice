62374642655f82a015148fe6d1335701dcdded94
Victor <monstervictor@gmail.com>
2015-10-15 20:08:25 -0400

    CameraShake done

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Camera/CameraShaker.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Camera/CameraShaker.cs
@@ -6,12 +6,11 @@ using Random = UnityEngine.Random;
 public class CameraShaker : MonoBehaviour {
 
     public bool shakePosition;
-    private bool shakeRotation = false;
+    //private bool shakeRotation = false; //NotImplemented
 
-    public float _shakeIntensity = 1f;
-    public float shakeDecay = 0.02f;
-    public float shakeTime = 1f;
-    public float _mitigationOnAddition = 0.3f;
+    public float _shakeIntensity = 1.5f;
+    public float shakeTime = 0.5f;
+    public float _mitigationOnShakeAddition = 0.2f;
     public float _shakeFlatConstant = 2f;
 
     private bool isShakeRunning = false;
@@ -23,7 +22,7 @@ public class CameraShaker : MonoBehaviour {
         if (isShakeRunning)
         {
             StopCoroutine(_currentShake);
-            startingIntensity += _shakeIntensity * _mitigationOnAddition;
+            startingIntensity += _shakeIntensity * _mitigationOnShakeAddition;
             isShakeRunning = false;
         }
         else
@@ -40,36 +39,38 @@ public class CameraShaker : MonoBehaviour {
     private Vector3 _originalPosition;
     IEnumerator ProcessShake(float shakeIntensity, float startingIntensity)
     {
-        Debug.Log(shakeIntensity + " " + startingIntensity);
+        if (startingIntensity == 0f) startingIntensity = 0.0001f;
         if (!isShakeRunning)
         {
             _currentIntensity = startingIntensity;
             isShakeRunning = true;
-            //var originalPosition = transform.position;
             var originalRotation = transform.rotation;
             float currentShakeIntensity = _currentIntensity;
             var targetTime = Time.time + shakeTime;
             bool isIncreasing = true;
             while (Time.time < targetTime)
             {
+                Debug.Log(shakeIntensity + " " + currentShakeIntensity);
                 if (shakePosition)
                 {
                     transform.position = _originalPosition + Random.insideUnitSphere * (AddPerlinNoiseTo(shakeIntensity) * currentShakeIntensity);
                 }
-                if (shakeRotation)
+                /*if (shakeRotation)
                 {
                     transform.rotation = new Quaternion(originalRotation.x + AddPerlinNoiseTo(Random.Range(-currentShakeIntensity, currentShakeIntensity)) * .2f,
                                                         originalRotation.y + AddPerlinNoiseTo(Random.Range(-currentShakeIntensity, currentShakeIntensity)) * .2f,
                                                         originalRotation.z + AddPerlinNoiseTo(Random.Range(-currentShakeIntensity, currentShakeIntensity)) * .2f,
                                                         originalRotation.w + AddPerlinNoiseTo(Random.Range(-currentShakeIntensity, currentShakeIntensity)) * .2f);
-                }
+                }*/
                 if (isIncreasing)
                 {
-                    currentShakeIntensity += (startingIntensity/(targetTime - Time.time))*2;
+                    currentShakeIntensity += (startingIntensity/(shakeTime*1000))*2f;
                     isIncreasing = currentShakeIntensity < shakeIntensity;
                 }
-                else currentShakeIntensity -= (shakeIntensity/(targetTime - Time.time))*2;
-                //currentShakeIntensity -= shakeDecay;
+                else
+                {
+                    currentShakeIntensity -= (shakeIntensity/(shakeTime*1000))*2f;
+                }
                 yield return null;
             }
             transform.position = _originalPosition;
