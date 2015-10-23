36d75a9f7b284668537a7a28f8d28d9e2ccc17bd
symbol24 <phillip.conte@gmail.com>
2015-10-22 20:31:32 -0400

    Revert "i might have to roll back"
    This reverts commit 5962cc5190d68f83bfac902abbb5a39bec543d7d.

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/PurpleTimer.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/PurpleTimer.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class PurpleTimer : MonoBehaviour {
     [SerializeField] private Animator m_purpleAnimator;
-    private RoundStartTimer m_roundMenu;
-    private int m_animCounter = 0;
 
 	// Use this for initialization
 	void Start () {
-        m_roundMenu = FindObjectOfType<RoundStartTimer>();
+	
 	}
 	
 	// Update is called once per frame
@@ -18,12 +16,6 @@ public class PurpleTimer : MonoBehaviour {
 
     public void AnimationEnded()
     {
-        if (m_animCounter == m_roundMenu.AmountOfSeconds)
-            SoundManager.PlaySFX(m_roundMenu.GoClipName);
-        else
-            SoundManager.PlaySFX(m_roundMenu.NumberClipName);
         m_purpleAnimator.SetBool("Play", false);
-        m_animCounter++;
-        if (m_animCounter > m_roundMenu.AmountOfSeconds) ;
     }
 }
