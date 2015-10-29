bf1ecd457db96082203f3377268612af2c934fcb
symbol24 <phillip.conte@gmail.com>
2015-10-28 23:51:52 -0400

    round menu is now animated!

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/SubForAnimations/RoundMenuSub.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/SubForAnimations/RoundMenuSub.cs
@@ -6,12 +6,18 @@ public class RoundMenuSub : MonoBehaviour {
 
     public void ResetGoUp()
     {
-
+        m_parent.UpdateBoolAnimator("GoUp", false);
+        m_parent.TriggerAfterGoUp();
     }
 
     public void ResetGoDown()
     {
+        m_parent.UpdateBoolAnimator("GoDown", false);
+    }
 
+    public void StartOfGoUp()
+    {
+        m_parent.TriggerOnStartOfGoUp();
     }
 
 }
