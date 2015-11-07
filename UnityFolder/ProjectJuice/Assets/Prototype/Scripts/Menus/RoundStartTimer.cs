ad1609fcecb600701827eba87f2839f7ac771e22
symbol24 <phillip.conte@gmail.com>
2015-11-07 15:42:54 -0500

    round start timer now enters and exits teh screen like the other panels yay

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/RoundStartTimer.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/Menus/RoundStartTimer.cs
@@ -11,6 +11,7 @@ public class RoundStartTimer : MonoBehaviour {
     public int AmountOfSeconds { get { return m_amountOfSeconds; } }
     private LoadingScreen m_fader;
     [SerializeField] private Animator m_PurpleAnimator;
+    [SerializeField] private Animator m_panelAnimator;
     [HideInInspector] public string NumberClipName;
     [HideInInspector] public string GoClipName;
     [HideInInspector] public string GetReadyClipName;
@@ -18,14 +19,24 @@ public class RoundStartTimer : MonoBehaviour {
     // Use this for initialization
     void Start () {
         if (m_fader == null) m_fader = FindObjectOfType<LoadingScreen>();
-        if(m_fader != null) m_fader.LoadingAnimDone += StartCountdown;
+        if(m_fader != null) m_fader.LoadingAnimDone += EnterTimer;
         m_TimerText.text = Database.instance.GameTexts[16];
         GameManager.instance.InjectRoundStartTimer(this);
     }
 
-    private void StartCountdown(object sender, System.EventArgs e)
+    private void EnterTimer(object sender, System.EventArgs e)
+    {
+        m_panelAnimator.SetBool("Enter", true);
+    }
+
+    public void StartTimer()
     {
         StartCoroutine(DisplayTime());
+    }
+
+    public void SetBool(Animator anim, string with, bool what)
+    {
+        anim.SetBool(with, what);
     }
 
     // Update is called once per frame
@@ -42,19 +53,25 @@ public class RoundStartTimer : MonoBehaviour {
     {
         bool displayed = false;
         int time = m_amountOfSeconds;
-        m_PurpleAnimator.SetBool("Play", true);
+        SetBool(m_PurpleAnimator, "Play", true);
         while (!displayed)
         {
-            m_PurpleAnimator.SetBool("Play", true);
+            SetBool(m_PurpleAnimator, "Play", true);
             m_TimerText.text = time.ToString();
             time--;
             if (time == 0) displayed = true;
             yield return new WaitForSeconds(m_delayBetweenNumbers);
         }
-        m_PurpleAnimator.SetBool("Play", true);
+        SetBool(m_PurpleAnimator, "Play", true);
         m_TimerText.text = Database.instance.GameTexts[17];
-        yield return new WaitForSeconds(m_delayAfterGo);
+        yield return new WaitForSeconds(m_delayAfterGo/2);
+        SetBool(m_panelAnimator, "Exit", true);
+        yield return new WaitForSeconds(m_delayAfterGo/2);
         GameManager.instance.SetGameState(GameState.Playing);
+    }
+
+    public void Deactivate()
+    {
         gameObject.SetActive(false);
     }
 }
