4740bfde3ffa15e96ca3b9e5ac10e099715b6ca4
symbol24 <phillip.conte@gmail.com>
2015-11-14 15:58:27 -0500

    robot flickering eye!

--- UnityFolder/ProjectJuice/Assets/Prototype/Scripts/HP/HPScript.cs
+++ UnityFolder/ProjectJuice/Assets/Prototype/Scripts/HP/HPScript.cs
@@ -19,7 +19,14 @@ public class HPScript : HPBase
     [Range(0,20)][SerializeField] private int _raycastIterationsToFindTarget = 5;
     [SerializeField] private Transform _centerOfReferenceForJuice;
     [SerializeField] private Rigidbody2D _mainRigidbody;
+    [SerializeField] private GameObject _robotEyeToFlicker;
+    [Range(0,1)] [SerializeField] private float _shrinkAmount = 0.2f;
+    [Range(0,5)] [SerializeField] private int _flickerAmount = 3;
+    [Range(0,1)] [SerializeField] private float _flickerTime = 0.2f;
+    private bool _flickering = false;
 
+    private float _originalEyeY;
+ 
     private bool m_ShieldImmunity = false;
     public bool ShieldImunity { get { return m_ShieldImmunity; } }
     public void SwitchShieldImunity()
@@ -58,6 +65,37 @@ public class HPScript : HPBase
 
         return true;
     }
+    
+    private void StartFlicker()
+    {
+        StartCoroutine(FlickerEye());
+    }
+
+    private IEnumerator FlickerEye()
+    {
+        if (_robotEyeToFlicker != null)
+        {
+            if (!_flickering) {
+                _flickering = true;
+                _originalEyeY = _robotEyeToFlicker.transform.localScale.y;
+                float newY = (1 - _shrinkAmount) * _originalEyeY;
+                Transform _updatedTransform = _robotEyeToFlicker.transform;
+                _updatedTransform.localScale = new Vector3(_updatedTransform.localScale.x, newY, _updatedTransform.localScale.z);
+                yield return new WaitForSeconds(_flickerTime);
+                for (int i = 0; i < _flickerAmount; i++)
+                {
+                    _robotEyeToFlicker.SetActive(false);
+                    yield return new WaitForSeconds(_flickerTime);
+                    _robotEyeToFlicker.SetActive(true);
+                    yield return new WaitForSeconds(_flickerTime);
+                }
+                _updatedTransform.localScale = new Vector3(_updatedTransform.localScale.x, _originalEyeY, _updatedTransform.localScale.z);
+                _flickering = false;
+            }
+        }
+        else Debug.LogError("HPscript does not have the eye assigned for " + gameObject.name);
+        yield return 0;
+    }
 
     protected virtual void OnHpImpactReceived(ImpactEventArgs e)
     {
@@ -95,6 +133,11 @@ public class HPScript : HPBase
     private void OnHpChanged(object sender, HpChangedEventArgs hpChangedEventArgs)
     {
         if (CurrentHp <= 0) StartCoroutine(DestroyNextFrame());
+        else if(CurrentHp > 0)
+        {
+            if (hpChangedEventArgs.PreviousHp > hpChangedEventArgs.NewHp)
+                StartFlicker();
+        }
     }
 
     private IEnumerator DestroyNextFrame()
